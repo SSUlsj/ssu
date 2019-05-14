51aa5cb7bacea1dc821e62e61ed241f04b5d526a
SSUlsj <sj7288sj@gmail.com>
2019-05-14 18:13:18 +0900

    맵 하나로 통합`

--- mapload.c
+++ mapload.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 int main()
 {
-	FILE * map;
-	map = fopen("map5.txt","r");
-	char text;
-
-	if (map == NULL){
+	FILE * hi[5];
+	char map[5];
+	for (int i = 0; i <= 4; i++){
+		hi[i] = fopen("map1.txt","r")
+	}
+	if (hi == NULL){
 		printf("불러올 파일이 없습니다.");
 	}
 	else{
-		while(fscanf(map,"%c",&text) != EOF){
+		while(fscanf(map,"%c",&map[]) != EOF){
 			printf("%c",text);
 		}
 	}
