67105b9ca156a38be28522fe7a0177753cbc73c5
SSUlsj <sj7288sj@gmail.com>
2019-05-14 18:53:16 +0900

    맵 변경/맵 로드 변경/포지션 작업 중

--- mapload.c
+++ mapload.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
 int main()
 {
-	FILE * hi[5];
-	char map[5];
-	for (int i = 0; i <= 4; i++){
-		hi[i] = fopen("map1.txt","r")
-	}
-	if (hi == NULL){
+	FILE * map = fopen("map.txt","r");
+	char text;
+	if (map == NULL){
 		printf("불러올 파일이 없습니다.");
 	}
 	else{
-		while(fscanf(map,"%c",&map[]) != EOF){
+		while(fscanf(map,"%c",&text) != EOF){
 			printf("%c",text);
 		}
 	}
--- position.c
+++ position.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main()
 {
-	int x = 0;
-	int y = 0;
-
+	int cha_x = 0;
+	int cha_y = 0;
+	char pos[5][cha_x][cha_y];
+	FILE *map = fopen("map.txt"
 
