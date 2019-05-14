2b5b0563f41637cd7bc99fd6c102d3572c19a713
SSUlsj <sj7288sj@gmail.com>
2019-05-14 15:56:03 +0900

    변경사항 있음

---
+++ testfile.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+void username()
+{
+    char user[10];
+
+    printf("소코반 게임\n");
+    printf("Insert your name : ");
+    do {
+        scanf("%s",&user);
+        if (strlen(user) > 10) {
+            printf("Maximum name is 10 words.\n");
+            printf("Insert your name : ");
+        }
+
+
+    } while (strlen(user) >10);
+
+    printf("   Hello %s",user);
+
+    return;
+}
+
+int main()
+{
+	username();
+	return 0;
+}
