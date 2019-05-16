28a2162ec46507b8687846f9a5505a538a34fe95
gonagi <kkh10002@naver.com>
2019-05-16 15:06:46 +0900

    hjkl 방향키 마지막 수정..?

--- hjkl.c
+++ hjkl.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
-void Player_Move_Action();
 #define h 75 // h : 왼쪽
 #define j 80 // j : 아래
 #define k 72 // k : 위
 #define l 77 // l : 오른쪽
-int main()
+void Player_Move_Action()
 {
 	int key;
 	int x = 0;
