251f215f40d3afc33ce4295cf23b3e5e581664da
gonagi <kkh10002@naver.com>
2019-05-16 14:52:55 +0900

    hjkl추가

--- hjkl.c
+++ hjkl.c
@@ -1,4 +1,36 @@
+#include<stdio.h>
+void Player_Move_Action();
 #define h 75 // h : 왼쪽
 #define j 80 // j : 아래
 #define k 72 // k : 위
 #define l 77 // l : 오른쪽
+int main()
+{
+	int key;
+	int x = 0;
+	int y = 0;
+	while(1)
+	{
+
+		key = getchar();
+
+		switch(key)
+		{
+		case h: //왼쪽
+			x = 1;
+			break;
+		case j: //아래
+			y = -1;
+			break;
+		case k: //위
+			y = 1;
+			break;
+		case l: //오른쪽
+			x = -1;
+			break;
+		}
+	}
+}
+	
+
+
