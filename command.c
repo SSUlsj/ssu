1f7a561a3dfd54a48aa4e460c3033c215bff5b61
neoskyclad <neoskyclad@naver.com>
2019-05-22 00:46:11 +0900

    화면 초기화하는 명령어 사용해서 조금 수정했습니다.

--- command.c
+++ command.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
-#include "getch.h"
+#include <stdlib.h>
+#include <termio.h>
+int getch()
+{
+        int ch;
+
+        struct termios buf;
+        struct termios save;
+
+        tcgetattr(0, &save);
+        buf = save;
+
+        buf.c_lflag&=~(ICANON|ECHO);
+        buf.c_cc[VMIN] = 1;
+        buf.c_cc[VTIME] = 0;
+
+        tcsetattr(0, TCSAFLUSH, &buf);
+
+        ch=getchar();
+        tcsetattr(0, TCSAFLUSH, &save);
+
+        return ch;
+}
+
 int main()
 {
 	int ch;
+	int check=1;
+
+	system("clear");
 
 	printf("(command) : ");
 
-	while(1){
+	while(check){
 	ch=getch();
 	if (ch=='d'){
+		system("clear");
 		printf("h(왼쪽),j(아래),k(위),l(오른쪽)\n");
 		printf("u(undo)\n");
 		printf("r(replay)\n");
@@ -19,8 +46,9 @@ int main()
 		printf("t(top)\n");
 	}
 	if (ch=='e'){
+		system("clear");
 		printf("\nS E E Y O U (username) . . . .\n");
-		return 0;
+		check=0;
 	}
 }
 return 0;
