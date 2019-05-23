14b10382c9117ece926e4fefd6794a725f85f4ef
SSUlsj <sj7288sj@gmail.com>
2019-05-23 17:00:56 +0900

    50%정도 진행된 것 같음

--- testfile.c
+++ testfile.c
@@ -1,21 +1,100 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <termio.h>
 
 int stage, x, y = 0;
 int stage_y[5] = {0};
+int stage_x[5] = {0};
 int box_count, chest_count = 0;
+int char_x, char_y = 0;
+
 char map_pos[5][30][30] = {};
 char text;
-
 char user[10];
 
 void username();
-void mapload();
+int mapload();
+void testmove();
+void mapprint();
+
+int getch()
+{
+        int ch;
+
+        struct termios buf;
+        struct termios save;
+
+        tcgetattr(0, &save);
+        buf = save;
+
+        buf.c_lflag&=~(ICANON|ECHO);
+        buf.c_cc[VMIN] = 1;
+        buf.c_cc[VTIME] = 0;
+
+        tcsetattr(0, TCSAFLUSH, &buf);
+
+        ch=getchar();
+        tcsetattr(0, TCSAFLUSH, &save);
+
+        return ch;
+}
+
+void testmove()
+{
+    int check=1;
+    int input_char;
+    int cnt=0;
+
+    system("clear\n");
+
+    while(check){
+		
+        mapprint();
+		printf("Move Attempts : %d\n",cnt);
+
+        input_char = getch();
+        switch(input_char){
+            case 'h' :
+                map_pos[0][char_y][char_x-1]='@';
+                map_pos[0][char_y][char_x]='.';
+                cnt++;
+                break;
+            case 'j' :
+                map_pos[0][char_y+1][char_x]='@';
+                map_pos[0][char_y][char_x]='.';
+                cnt++;
+                break;
+            case 'k' :
+                map_pos[0][char_y-1][char_x]='@';
+                map_pos[0][char_y][char_x]='.';
+                cnt++;
+                break;
+            case 'l' :
+                map_pos[0][char_y][char_x+1]='@';
+                map_pos[0][char_y][char_x]='.';
+                cnt++;
+                break;
+            case 'r' :
+                mapload();
+                cnt=0;
+                break;
+
+            case 'e' :
+                check=0;
+                break;
+            default :
+                break;
+		}
+        system("clear");
+		printf("Hello %s\n",user);
+    }
+    printf("\n");
+    return;
+}
 
 void username()
 {
-    char user[10];
     printf("Start....\ninput name : ");
 name :
     while (1){
@@ -34,12 +113,11 @@ name :
         }
         break;
     }
-    printf("   Hello %s",user);
     return;
 }
 
 
-void mapload()
+int mapload()
 {
     FILE * map = fopen("map.txt","r");
     while (fscanf(map, "%c", &text) != EOF){
@@ -62,20 +140,54 @@ void mapload()
             stage_y[stage] = y+1;
             break;
         }
+		else if(text == '@'){
+			map_pos[stage][y][x] = text;
+			x++;
+		}
+		else if(text == '$'){
+			map_pos[stage][y][x] = text;
+			box_count++;
+			x++;
+		}
+		else if(text == 'O'){
+			map_pos[stage][y][x] = text;
+			chest_count++;
+			x++;
+		}
         else{
-            map_pos[stage][x][y] = text;
+            map_pos[stage][y][x] = text;
             x++;
         }
     }
+	if (box_count != chest_count){
+		printf("박스의 개수와 보관장소의 개수가 같지 않습니다.\n프로그램을 종료합니다.");
+		return 1;
+	}
     int fclose(FILE * map);
-    return;
+    return 0;
 }
 
+void mapprint()
+{
+	int y_pos, x_pos;
+	for (y_pos = 0; y_pos <= stage_y[0]; y_pos++){
+		for (x_pos = 0; x_pos <= 30; x_pos++){
+			printf("%c",map_pos[0][y_pos][x_pos]);
+			if (map_pos[0][y_pos][x_pos] == '@'){
+				char_x = x_pos;
+				char_y = y_pos;
+			}
+		}
+		printf("\n");
+	}
+}
 int main()
 {
 	system("clear");
 	username();
-	mapload();
-
+	if (mapload() == 1)
+		return 0;
+	testmove();
+	printf("See you %s",user);
 	return 0;
 }
